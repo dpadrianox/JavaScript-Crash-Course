668a5db33b784e8af5c5a3e26a369bd97fb4ea20
Princess <dpadrianox@gmail.com>
2025-05-01 23:13:26 -0400

    then promise

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-function toggleDarkMode () {
-    document.querySelector('body').classList.toggle("dark-theme")
-}
+// fetch("https://jsonplaceholder.typicode.com/users"))
+const emailRef = document.querySelector(".email");
+
+// // 1. Then
+// fetch("https://jsonplaceholder.typicode.com/users/1") //fetching data from the API(backend)
+//   .then((response) => {                              //unlocks the promise
+//     return response.json();                         //converts the response to JSON
+//   })
+//   .then((data) => {                                 //.then unlocks the promise response.json()
+//     console.log(data);                              //logs the data to the console
+//     emailRef.innerHTML = data.email;               //displays the email in the HTML
+// });
+
+// 2. Async/Await
+await fetch("https://jsonplaceholder.typicode.com/users/1")
